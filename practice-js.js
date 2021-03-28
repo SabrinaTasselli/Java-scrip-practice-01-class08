d9ac6902a79a618760e68e8b303127d6bc71f9df
Sabrina Tasselli <rodrigodandrea>
2021-03-28 18:39:10 -0300

    Practice01-js

---
+++ practice-js.js
@@ -0,0 +1,19 @@
+/* 1- Variables y Operadores
+a- Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos números en una 3er variable.
+*/
+var a = 5;
+var b = 10;
+var x = a + b;
+console.log (x)
+/* b- Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.
+*/
+var nombre = "Sabrina";
+var apellido = "Tasselli";
+var nombreCompleto = nombre + apellido;
+console.log (nombreCompleto)
+/* c- Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string) guardando el resultado la suma una 3er variable (utilizar length).
+*/
+var nombre = "Sabrina";
+var apellido = "Tasselli";
+var nombreCompleto = nombre + apellido;
+console.log (nombreCompleto.length)
